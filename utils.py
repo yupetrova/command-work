73327da612bb8e23ca83e6c8b4d0e462eabfb00d
Yuliia Petrova <152798272+yupetrova@users.noreply.github.com>
2024-02-14 20:09:47 +0200

    Update and rename empty.txt to utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+#factorial func
+
+def Factorial(n):
+    fact = 1
+    if n < 0:
+        raise ValueError("Please, input integer >= 0")
+    elif n == 0:
+        return 1
+    else:
+        for i in range(1, n + 1):
+            fact = fact * i
+        print(fact)
