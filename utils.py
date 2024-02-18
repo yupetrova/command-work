a73ecfd121329d52d037be3686b44adcf335f97a
Egor Orlik <egororlik27@knu,ua>
2024-02-18 15:16:32 +0200

    add cod

--- utils.py
+++ utils.py
@@ -10,3 +10,20 @@ def factorial(n):
         for i in range(1, n + 1):
             fact = fact * i
         print(fact)
+
+def power_five(num):
+    degree = 0
+    while 5 ** degree <= num:
+        if 5 ** degree == num:
+            return True
+        degree += 1
+
+    return False
+
+number = int(input("Введіть число: "))
+result = power_five(number)
+
+if result:
+    print(f"{number} є степенем п'ятірки.")
+else:
+    print(f"{number} не є степенем п'ятірки.")
